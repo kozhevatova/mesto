8e347a95d5ed6be216501b2ed6384b9810018dee
Anna Kozhevatova <ad.kozhevatova@yandex.ru>
2020-10-23 19:35:17 +0300

    fix: Popup (bind)

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -1,20 +1,21 @@
 export default class Popup {
   constructor({popupSelector}) {
     this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popup.classList.add('popup_opened');
 
     // событие нажатия на кнопку клавиатуры
-    document.addEventListener('keydown',this._handleEscClose.bind(this));
+    document.addEventListener('keydown',this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
 
     // событие нажатия на кнопку клавиатуры
-    document.removeEventListener('keydown',this._handleEscClose.bind(this));
+    document.removeEventListener('keydown',this._handleEscClose);
   }
 
   _handleEscClose(event) {
